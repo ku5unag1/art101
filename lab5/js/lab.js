86d16f5bab694590a7a9c7e1265bde239dc951d4
ku5unag1 <91301925+ku5unag1@users.noreply.github.com>
2021-10-19 01:24:25 -0700

    Initial commit

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+//Autor: Nkem Ndidi <nndidi@ucsc.edu>
+//Created: 18 October 2021
+//Lisence: Public Domain
+
+//Define Variables
+make = "Honda";
+model = "Civic";
+color = "Space Gray";
+year = "2016";
+
+//Calculate
+age = 2021 - year;
+
+//Output
+document.writeln("make: " + make + "<br>");
+document.writeln("model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + "<br>");
