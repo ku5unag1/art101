8a7962d3f3a3f21f790bbdd5ac2a59aa1991595e
ku5unag1 <91301925+ku5unag1@users.noreply.github.com>
2021-12-02 00:36:42 -0800

    lab 16

---
+++ lab16/js/lab.js
@@ -0,0 +1,34 @@
+//Author: Nkem Ndidi and Ashley Lu
+//Since 02 December 2021
+//Lisence: Public Domain
+//Summary: JSON and API
+//Description: The purpose of this lab was to experiment with processing JSON using APIs.
+
+//Declare api URL
+
+function comicObj() {
+  $.ajax({
+    // the url for the request
+    url: "https://xkcd.com/info.0.json",
+    //whether it is a POST or GET
+    type: "GET",
+    dataType: "JSON",
+    //when the api is successful
+    success: function(data) {
+      console.log("success: ", data);
+      var titleTxt = data.title;
+      $("#title").html(titleTxt);
+      var imgUrl = data.img;
+      var imgTag = "<img src=" + imgUrl + ">";
+      console.log(imgUrl, imgTag);
+      $("#image").html(imgTag);
+    },
+    //when the api is unsuccessful
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log("Error:", textStatus, errorThrown)
+    }
+    //what do we do when the api call is successful
+    //everything goes in here
+  })
+}
+$("#button").click(comicObj);
